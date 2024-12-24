5844f564a13f4b6df6308f3b01a230c35d96e9e4
Sam Taiwo <staiwo@onlydates.fan>
2024-12-24 21:38:42 +0000

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+rootProject.name = "accessibility"
+rootProject.buildFileName = "engine.server.utils.accessibility.gradle.kts"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
